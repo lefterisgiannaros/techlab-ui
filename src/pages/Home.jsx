e67d4d7058c6066a96ef89a7ec075dbc105b6909
Lefteris Giannaros <47523060+lefterisgiannaros@users.noreply.github.com>
2023-08-22 23:58:00 +0300

    css changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React from "react";
 
 function Home() {
-  return <div>Home Content</div>;
+  return (
+    <div className="home-content d-flex vh-100">
+      <div className="flex-fill">
+        <h1 className="title">No More Excuses To Learn</h1>
+      </div>
+      <div className="flex-fill">
+        <div className="rect-orange"></div>
+        <div className="rect-green"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
