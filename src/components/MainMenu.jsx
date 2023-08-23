ecc528062aa229f3732c5fc44be5ae0976b8f0bd
Lefteris Giannaros <47523060+lefterisgiannaros@users.noreply.github.com>
2023-08-23 17:52:51 +0300

    final, could use some refactoring

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -5,19 +5,19 @@ class MainMenu extends React.Component {
   render() {
     return (
       <div className="main-menu">
-        <ul className="d-flex justify-content-between align-items-center w-100 list-unstyled p-5">
+        <ul className="d-flex justify-content-between align-items-center w-100 list-unstyled">
           <li>
             <img className="logo" src="/TechLab.svg"></img>
           </li>
           <div className="d-flex gap-4">
-          <NavLink to="/" activeClassName="active">Home</NavLink>
-          <NavLink to="/courses" activeClassName="active">Courses</NavLink>
-          <NavLink to="/features" activeClassName="active">Features</NavLink>
-          <NavLink to="/contact" activeClassName="active">Contact</NavLink>
+          <NavLink to="/" activeclassname="active">Home</NavLink>
+          <NavLink to="/courses" activeclassname="active">Courses</NavLink>
+          <NavLink to="/features" activeclassname="active">Features</NavLink>
+          <NavLink to="/contact" activeclassname="active">Contact</NavLink>
           </div>
 
           <li>
-            <button className="btn-signup">Sign Up</button>
+            <button className="btn-curved btn-white">Sign Up</button>
           </li>
         </ul>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,48 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="home-content d-flex vh-100">
-      <div className="flex-fill">
-        <h1 className="title">No More Excuses To Learn</h1>
+    <div className="home-content row">
+      <div className="content-left col-md-7">
+        <h1>No More</h1>
+        <h1>Excuses To Learn</h1>
+        <h4>
+          Learn anything you need and want to know through our online courses
+          from the very best mentors. Available 24 hours.
+        </h4>
+        <button className="btn-curved btn-darkgrey">Get Started</button>
+
+        <div className="stats-container">
+          <div className="stat">
+            <div className="txt-top">250+</div>
+            <div className="txt-bottom">Professional Mentors</div>
+          </div>
+          <div className="circle"></div>
+          <div className="stat">
+            <div className="txt-top">3000+</div>
+            <div className="txt-bottom">Course Materials</div>
+          </div>
+          <div className="circle"></div>
+          <div className="stat">
+            <div className="txt-top">1000+</div>
+            <div className="txt-bottom">Active Learners</div>
+          </div>
+        </div>
       </div>
-      <div className="flex-fill">
+
+      <div className="col-md-5 d-none d-xl-block position-relative">
+        <div className="blk-circle"></div>
+
+        <div className="semicircle">
+          <div className="small-circle"></div>
+        </div>
+
         <div className="rect-orange"></div>
         <div className="rect-green"></div>
+
+        <img className="ornament" src="/Ornament.svg"></img>
+
+        <div className="white-circle"></div>
+        <div className="small-white-circle"></div>
       </div>
     </div>
   );
